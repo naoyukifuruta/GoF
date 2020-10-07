27a0784398f02fccc2c0039d0c03ec354d77d7e6
nfrutta <naoyukifuruta@outlook.jp>
2020-10-07 09:58:36 +0900

    [mod]Iterator

--- src/Iterator/IIterator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Iterator
-{
-    public interface IIterator
-    {
-        public abstract bool HasNext();
-        public abstract Object Next();
-    }
-}
--- src/Iterator/Program.cs
+++ src/Iterator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Iterator
 {
@@ -18,6 +19,86 @@ namespace Iterator
                 var book = (Book)iterator.Next();
                 Console.WriteLine(book.Name);
             }
+        }
+    }
+
+    /** 便宜上1ファイルに全クラスを実装 */
+
+    public interface IAggregate
+    {
+        IIterator Iterator();
+    }
+
+    public interface IIterator
+    {
+        bool HasNext();
+        object Next();
+    }
+
+    public class Book
+    {
+        public string Name { get; set; }
+
+        public Book(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class BookShelf : IAggregate
+    {
+        private List<Book> _books = new List<Book>();
+
+        public Book GetBookAt(int index)
+        {
+            return _books[index];
+        }
+
+        public void AppendBook(Book book)
+        {
+            _books.Add(book);
+        }
+
+        public int GetLength()
+        {
+            return _books.Count;
+        }
+
+        public IIterator Iterator()
+        {
+            return new BookShelfIterator(this);
+        }
+    }
+
+    public class BookShelfIterator : IIterator
+    {
+        public BookShelf BookShelf { get; set; }
+
+        public int Index { get; set; }
+
+        public BookShelfIterator(BookShelf bookShelf)
+        {
+            BookShelf = bookShelf;
+            Index = 0;
+        }
+
+        public bool HasNext()
+        {
+            if (Index < BookShelf.GetLength())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object Next()
+        {
+            Book book = BookShelf.GetBookAt(Index);
+            Index++;
+            return book;
         }
     }
 }
