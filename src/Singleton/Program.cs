8e6ad8d578a5d4269072487f53be3117165822ef
Naoyuki Furuta <nfrutta1208@gmail.com>
2020-10-11 15:12:37 +0900

    [add]singleton

---
+++ src/Singleton/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start.");
+
+            Singleton obj1 = Singleton.GetInstance();
+            Singleton obj2 = Singleton.GetInstance();
+
+            if (obj1 == obj2)
+            {
+                Console.WriteLine("Same Instances.");
+            }
+
+            Console.WriteLine("End.");
+        }
+    }
+
+    public class Singleton
+    {
+        private static Singleton singleton = new Singleton();
+
+        private Singleton()
+        {
+            Console.WriteLine("Create Instance.");
+        }
+
+        public static Singleton GetInstance()
+        {
+            return singleton;
+        }
+    }
+}
