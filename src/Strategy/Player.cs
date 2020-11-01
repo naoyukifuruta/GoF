3b1ba73e61cab035bd56a30ef1d6adc6a40697c1
Naoyuki Furuta <nfrutta1208@gmail.com>
2020-11-01 14:49:51 +0900

    [mod]strategy

---
+++ src/Strategy/Player.cs
@@ -0,0 +1,47 @@
+﻿
+namespace Strategy
+{
+    public class Player
+    {
+        private string name;
+        private Strategy strategy;
+        private int wincount;
+        private int losecount;
+        private int gamecount;
+
+        public Player(string name, Strategy strategy)
+        {
+            this.name = name;
+            this.strategy = strategy;
+        }
+
+        public Hand NextHand()
+        {
+            return strategy.NextHand();
+        }
+
+        public void Win()
+        {
+            strategy.Study(true);
+            wincount++;
+            gamecount++;
+        }
+
+        public void Lose()
+        {
+            strategy.Study(false);
+            losecount++;
+            gamecount++;
+        }
+
+        public void Even()
+        {
+            gamecount++;
+        }
+
+        public override string ToString()
+        {
+            return $"[{name}:{gamecount} games, {wincount} win, {losecount} lose]";
+        }
+    }
+}
